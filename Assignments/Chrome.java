c69624d790f21dc0d6834a5a61b881e12ad9ce31
drajendarn <75475343+drajendarn@users.noreply.github.com>
2025-02-04 15:48:36 +0530

    Add files via upload

---
+++ Assignments/Chrome.java
@@ -0,0 +1,20 @@
+package week1.Assignments;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+
+		float version = 91.0f;
+		String dev = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortCutkey = 'C';
+		System.out.println("Chrome Version : " + version);
+		System.out.println("Developer : " + dev);
+		System.out.println("Is Beta value : " + isBeta);
+		System.out.println("Release year : " + releaseYear);
+		System.out.println("Shortcut Key : " + shortCutkey);
+
+	}
+
+}
