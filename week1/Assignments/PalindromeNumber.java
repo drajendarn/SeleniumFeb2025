98176ff1de9976c8b06e2792484571e67aed1fdc
drajendarn <75475343+drajendarn@users.noreply.github.com>
2025-02-04 15:50:11 +0530

    Add files via upload

---
+++ week1/Assignments/PalindromeNumber.java
@@ -0,0 +1,28 @@
+package week1.Assignments;
+
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+		
+		// Palindrome - number reads the same backward and forward ex) 121, 12321
+
+		int num = 12321;
+		int originalNumer = num;
+		int revNumber = 0;
+		while (num > 0) // Using for loop : for (;num!=0;)
+		{
+			revNumber = (revNumber * 10) + (num % 10);
+			num = num / 10;
+		}
+		System.out.println("Reversed number : " + revNumber);
+
+		if (originalNumer == revNumber)
+		{
+			System.out.println("The given number " + originalNumer + " is a palindrome");
+		} 
+		else
+			System.out.println("The given number " + originalNumer + " is a palindrome");
+
+	}
+
+}
