b2eaa6bc4cb9d3d08ddc529d8311c24f77ae5ef0
drajendarn <75475343+drajendarn@users.noreply.github.com>
2025-03-10 16:08:07 +0530

    Update AssAmazonActions.java

--- week3/day4/actionsclass/AssAmazonActions.java
+++ week3/day4/actionsclass/AssAmazonActions.java
@@ -21,67 +21,72 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.io.File;
 import java.io.IOException;
 
-
 import dev.failsafe.internal.util.Durations;
 
 public class AssAmazonActions {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		//To open Incognito browser
-		ChromeOptions options=new ChromeOptions();
+		// To open Incognito browser
+		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--incognito");
-		
-		ChromeDriver driver=new ChromeDriver(options);
+
+		ChromeDriver driver = new ChromeDriver(options);
 		driver.get("https://www.amazon.in/");
 		driver.navigate().refresh();
 		driver.navigate().refresh();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
-		//launch amazon app Search for "oneplus 9 pro".
+
+		// launch amazon app Search for "oneplus 9 pro".
 		WebElement element = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		WebElement search = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
-		
-		Actions action=new Actions(driver);
-		action.moveToElement(element).sendKeys(element, "oneplus 9 pro").click(search).perform();// sendkeys char seq is not working
-		
-		//Get the price of the first product
+
+		Actions action = new Actions(driver);
+		action.moveToElement(element).sendKeys(element, "oneplus 9 pro").click(search).perform();// sendkeys char seq is
+																									// not working
+
+		// Get the price of the first product
 		String text = driver.findElement(By.xpath("(//span[contains(@class,'a-price-whole')])[2]")).getText();
-		System.out.println("price of the first product :" + text );
-		
-		//Print the number of customer ratings for the first displayed product
+		System.out.println("price of the first product :" + text);
+
+		// Print the number of customer ratings for the first displayed product
 		WebElement rating = driver.findElement(By.xpath("(//a[contains(@aria-label,'ratings')])[1]"));
 		System.out.println("number of customer ratings for the first displayed product : " + rating.getText());
-		
-		//Click the first text link of the first image
-		WebElement firstlink = driver.findElement(By.xpath("(//a[contains(@class,'a-link-normal s-line-clamp-2 s-link-style a-text-normal')])[1]"));
+
+		// Click the first text link of the first image
+		WebElement firstlink = driver.findElement(
+				By.xpath("(//a[contains(@class,'a-link-normal s-line-clamp-2 s-link-style a-text-normal')])[1]"));
 		action.moveToElement(firstlink).click().perform();
-		
-		//Take a screenshot of the product displayed
+
+		// Take a screenshot of the product displayed
 		Set<String> windowHandles = driver.getWindowHandles();
 		List<String> list = new ArrayList<>(windowHandles);
-		String parentWindow=list.get(0);
-		String childWindow=list.get(1);
+		String parentWindow = list.get(0);
+		String childWindow = list.get(1);
 		driver.switchTo().window(childWindow);
-		
+
 		Thread.sleep(1000);
 		File src = driver.getScreenshotAs(org.openqa.selenium.OutputType.FILE);
-        File des= new File("C:\\Users\\DIVYA\\Desktop\\Selenium Feb2025\\Selenium\\src\\main\\resources\\Snapshot\\Amazon.png");
+		File des = new File(
+				"C:\\Users\\DIVYA\\Desktop\\Selenium Feb2025\\Selenium\\src\\main\\resources\\Snapshot\\Amazon.png");
 		FileUtils.copyFile(src, des);
-				
-		//click on Add to cart
+
+		// click on Add to cart
 		WebElement addCart = driver.findElement(By.xpath("(//input[@id='add-to-cart-button'])[2]"));
 		action.moveToElement(addCart).click().perform();
-		
-		
-		//Get cart subtotal
+
+		// Get cart subtotal
 		String windowHandle = driver.getWindowHandle();
-        driver.switchTo().window(windowHandle);						
-		String subTotal = driver.findElement(By.xpath("//span[@id='attach-accessory-cart-subtotal']")).getText();
-		System.out.println("cart subtotal : " + subTotal);//This returns blank value
+		driver.switchTo().window(windowHandle);
+		Thread.sleep(1000);
+		String subTotal = driver.findElement(By.xpath(
+				"//div[@class='a-fixed-left-grid-col a-col-right']//descendant::span[@id='attach-accessory-cart-subtotal']"))
+				.getText();
+
+		System.out.println("cart subtotal : " + subTotal);
+
 		Thread.sleep(1000);
 		System.out.println(driver.getTitle());
-		System.out.println("Test");
 		driver.quit();
 
 	}
